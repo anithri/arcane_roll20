1525653eb31b1ce2f79edca1297cdd8cb3e6685b
Scott P <anithri@gmail.com>
2014-06-18 03:34:56 -0600

    Initial setup after fork.

---
+++ dist/arcane_rol20.js
@@ -0,0 +1,22 @@
+/* arcane_rol20 - 0.0.1
+ * Useful scripts and stuff for Roll20's api
+ * https://github.com/anithri/aracne_roll20
+ */
+(function() {
+  (function() {
+    var root, _;
+    root = this;
+    _ = root._ || require('underscore');
+    return root.sumTo = function(n) {
+      var _i, _results;
+      return _((function() {
+        _results = [];
+        for (var _i = 1; 1 <= n ? _i <= n : _i >= n; 1 <= n ? _i++ : _i--){ _results.push(_i); }
+        return _results;
+      }).apply(this)).inject(function(sum, i) {
+        return sum + i;
+      }, 0);
+    };
+  })();
+
+}).call(this);
